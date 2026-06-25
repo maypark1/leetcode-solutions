699cf34dc5d28195d90f961862d1bb8d202bf1f3
maypark1 <parkchanmi801@gmail.com>
2026-06-26 06:39:34 +0900

    test: retest 13

--- solutions/13_roman_to_integer_easy.py
+++ solutions/13_roman_to_integer_easy.py
@@ -1,16 +1,26 @@
-# time: O(n)
-# space: O(1)
-# spent: 30
 #
 # @lc app=leetcode id=13 lang=python3
 #
 # [13] Roman to Integer
 #
+# time: O(n)
+# space: O(1)
+# spent: 20
+# runtime: 52ms
+# memory: 16.8MB
 
 # @lc code=start
 class Solution:
     def romanToInt(self, s: str) -> int:
-        
+        roman = {
+            'I': 1, 'V': 5, 'X': 10, 'L': 50,
+            'C': 100, 'D': 500, 'M': 1000
+        }
+        result = 0
+        for i in range(len(s)):
+            if i + 1 < len(s) and roman[s[i]] < roman[s[i+1]]:
+                result -= roman[s[i]]
+            else:
+                result += roman[s[i]]
+        return result
 # @lc code=end
-
-# sfasdfsdfadfa
