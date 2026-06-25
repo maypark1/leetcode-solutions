0d0728895bcd3554df867a917804d27fcd2cdf1a
maypark1 <parkchanmi801@gmail.com>
2026-06-25 19:58:06 +0900

    solve: 0020 valid parentheses

---
+++ solutions/20_valid_parentheses_easy.py
@@ -0,0 +1,23 @@
+#
+# @lc app=leetcode id=20 lang=python3
+#
+# [20] Valid Parentheses
+#
+# time: O(n)
+# space: O(n)
+# spent: 15
+
+# @lc code=start
+class Solution:
+    def isValid(self, s: str) -> bool:
+        stack = []
+        mapping = {')': '(', '}': '{', ']': '['}
+        for char in s:
+            if char in mapping:
+                top = stack.pop() if stack else '#'
+                if mapping[char] != top:
+                    return False
+            else:
+                stack.append(char)
+        return not stack
+# @lc code=end
